8703566855f1a5e4921ba46cd51082ffa5a1f239
John Pili <mrprintedwall@gmail.com>
2020-02-28 14:01:59 +0800

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace howto2002181
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn1HideRadio = new System.Windows.Forms.RadioButton();
+            this.btn1ShowRadio = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn3HideRadio = new System.Windows.Forms.RadioButton();
+            this.btn3ShowRadio = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btn2HideRadio = new System.Windows.Forms.RadioButton();
+            this.btn2ShowRadio = new System.Windows.Forms.RadioButton();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btn4HideRadio = new System.Windows.Forms.RadioButton();
+            this.btn4ShowRadio = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(74, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(74, 167);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 36);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(74, 209);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 36);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn1HideRadio);
+            this.groupBox1.Controls.Add(this.btn1ShowRadio);
+            this.groupBox1.Location = new System.Drawing.Point(263, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(225, 105);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Button1 Control";
+            // 
+            // btn1HideRadio
+            // 
+            this.btn1HideRadio.AutoSize = true;
+            this.btn1HideRadio.Location = new System.Drawing.Point(129, 46);
+            this.btn1HideRadio.Name = "btn1HideRadio";
+            this.btn1HideRadio.Size = new System.Drawing.Size(67, 24);
+            this.btn1HideRadio.TabIndex = 7;
+            this.btn1HideRadio.Text = "Hide";
+            this.btn1HideRadio.UseVisualStyleBackColor = true;
+            this.btn1HideRadio.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
+            // 
+            // btn1ShowRadio
+            // 
+            this.btn1ShowRadio.AutoSize = true;
+            this.btn1ShowRadio.Checked = true;
+            this.btn1ShowRadio.Location = new System.Drawing.Point(32, 46);
+            this.btn1ShowRadio.Name = "btn1ShowRadio";
+            this.btn1ShowRadio.Size = new System.Drawing.Size(74, 24);
+            this.btn1ShowRadio.TabIndex = 6;
+            this.btn1ShowRadio.TabStop = true;
+            this.btn1ShowRadio.Text = "Show";
+            this.btn1ShowRadio.UseVisualStyleBackColor = true;
+            this.btn1ShowRadio.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn3HideRadio);
+            this.groupBox2.Controls.Add(this.btn3ShowRadio);
+            this.groupBox2.Location = new System.Drawing.Point(263, 169);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(225, 105);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Button3 Control";
+            // 
+            // btn3HideRadio
+            // 
+            this.btn3HideRadio.AutoSize = true;
+            this.btn3HideRadio.Location = new System.Drawing.Point(129, 46);
+            this.btn3HideRadio.Name = "btn3HideRadio";
+            this.btn3HideRadio.Size = new System.Drawing.Size(67, 24);
+            this.btn3HideRadio.TabIndex = 7;
+            this.btn3HideRadio.Text = "Hide";
+            this.btn3HideRadio.UseVisualStyleBackColor = true;
+            this.btn3HideRadio.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
+            // 
+            // btn3ShowRadio
+            // 
+            this.btn3ShowRadio.AutoSize = true;
+            this.btn3ShowRadio.Checked = true;
+            this.btn3ShowRadio.Location = new System.Drawing.Point(32, 46);
+            this.btn3ShowRadio.Name = "btn3ShowRadio";
+            this.btn3ShowRadio.Size = new System.Drawing.Size(74, 24);
+            this.btn3ShowRadio.TabIndex = 6;
+            this.btn3ShowRadio.TabStop = true;
+            this.btn3ShowRadio.Text = "Show";
+            this.btn3ShowRadio.UseVisualStyleBackColor = true;
+            this.btn3ShowRadio.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btn2HideRadio);
+            this.groupBox3.Controls.Add(this.btn2ShowRadio);
+            this.groupBox3.Location = new System.Drawing.Point(504, 43);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(225, 105);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Button2 Control";
+            // 
+            // btn2HideRadio
+            // 
+            this.btn2HideRadio.AutoSize = true;
+            this.btn2HideRadio.Location = new System.Drawing.Point(129, 46);
+            this.btn2HideRadio.Name = "btn2HideRadio";
+            this.btn2HideRadio.Size = new System.Drawing.Size(67, 24);
+            this.btn2HideRadio.TabIndex = 7;
+            this.btn2HideRadio.Text = "Hide";
+            this.btn2HideRadio.UseVisualStyleBackColor = true;
+            this.btn2HideRadio.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
+            // 
+            // btn2ShowRadio
+            // 
+            this.btn2ShowRadio.AutoSize = true;
+            this.btn2ShowRadio.Checked = true;
+            this.btn2ShowRadio.Location = new System.Drawing.Point(32, 46);
+            this.btn2ShowRadio.Name = "btn2ShowRadio";
+            this.btn2ShowRadio.Size = new System.Drawing.Size(74, 24);
+            this.btn2ShowRadio.TabIndex = 6;
+            this.btn2ShowRadio.TabStop = true;
+            this.btn2ShowRadio.Text = "Show";
+            this.btn2ShowRadio.UseVisualStyleBackColor = true;
+            this.btn2ShowRadio.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btn4HideRadio);
+            this.groupBox4.Controls.Add(this.btn4ShowRadio);
+            this.groupBox4.Location = new System.Drawing.Point(504, 169);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(225, 105);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Button4 Control";
+            // 
+            // btn4HideRadio
+            // 
+            this.btn4HideRadio.AutoSize = true;
+            this.btn4HideRadio.Location = new System.Drawing.Point(129, 46);
+            this.btn4HideRadio.Name = "btn4HideRadio";
+            this.btn4HideRadio.Size = new System.Drawing.Size(67, 24);
+            this.btn4HideRadio.TabIndex = 7;
+            this.btn4HideRadio.Text = "Hide";
+            this.btn4HideRadio.UseVisualStyleBackColor = true;
+            this.btn4HideRadio.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
+            // 
+            // btn4ShowRadio
+            // 
+            this.btn4ShowRadio.AutoSize = true;
+            this.btn4ShowRadio.Checked = true;
+            this.btn4ShowRadio.Location = new System.Drawing.Point(32, 46);
+            this.btn4ShowRadio.Name = "btn4ShowRadio";
+            this.btn4ShowRadio.Size = new System.Drawing.Size(74, 24);
+            this.btn4ShowRadio.TabIndex = 6;
+            this.btn4ShowRadio.TabStop = true;
+            this.btn4ShowRadio.Text = "Show";
+            this.btn4ShowRadio.UseVisualStyleBackColor = true;
+            this.btn4ShowRadio.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 336);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ProgrammingPH.com";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton btn1HideRadio;
+        private System.Windows.Forms.RadioButton btn1ShowRadio;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton btn3HideRadio;
+        private System.Windows.Forms.RadioButton btn3ShowRadio;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton btn2HideRadio;
+        private System.Windows.Forms.RadioButton btn2ShowRadio;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RadioButton btn4HideRadio;
+        private System.Windows.Forms.RadioButton btn4ShowRadio;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace howto2002181
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton t = (RadioButton)sender;
+            switch (t.Name)
+            {
+                case "btn1ShowRadio":
+                    {
+                        button1.Visible = true;
+                        break;
+                    }
+                case "btn1HideRadio":
+                    {
+                        button1.Visible = false;
+                        break;
+                    }
+                case "btn2ShowRadio":
+                    {
+                        button2.Visible = true;
+                        break;
+                    }
+                case "btn2HideRadio":
+                    {
+                        button2.Visible = false;
+                        break;
+                    }
+                case "btn3ShowRadio":
+                    {
+                        button3.Visible = true;
+                        break;
+                    }
+                case "btn3HideRadio":
+                    {
+                        button3.Visible = false;
+                        break;
+                    }
+                case "btn4ShowRadio":
+                    {
+                        button4.Visible = true;
+                        break;
+                    }
+                case "btn4HideRadio":
+                    {
+                        button4.Visible = false;
+                        break;
+                    }
+            }
+        }
+    }
+}
